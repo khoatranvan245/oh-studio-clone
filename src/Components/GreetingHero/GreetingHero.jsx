43b4ae0bd94763a38054b8d4a61863ed33488634
Tran Van Khoa <khoatranvan245@gmail.com>
2024-02-05 21:18:31 +0700

    feat: add greeting hero to profile page

---
+++ src/Components/GreetingHero/GreetingHero.jsx
@@ -0,0 +1,10 @@
+import styles from './GreetingHero.module.css'
+const GreetingHero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1>Hey 👋🏼 I'm Oli </h1>
+    </div>
+  )
+}
+
+export default GreetingHero
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,11 @@
+import GreetingHero from '../Components/GreetingHero/GreetingHero'
 import NavBar from '../Components/Navbar/NavBar'
 
 const ProfilePage = () => {
     return (
         <>
             <NavBar currentPage='Profile'/>
+            <GreetingHero/>
         </>
     )
 }   
