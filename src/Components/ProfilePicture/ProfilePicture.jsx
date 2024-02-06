555e13a05139ac8dc3799dbb36ed7977778bd669
Tran Van Khoa <khoatranvan245@gmail.com>
2024-02-06 15:47:37 +0700

    style: add responsive to greeting hero

---
+++ src/Components/ProfilePicture/ProfilePicture.jsx
@@ -0,0 +1,12 @@
+import styles from './ProfilePicture.module.css'
+
+const ProfilePicture = () => {
+    return (
+        <div className={styles.profilePic}>
+            <div className={styles.pic1}></div>
+            <div className={styles.pic2}></div>
+        </div>
+    )
+}
+
+export default ProfilePicture
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,13 +1,15 @@
 import GreetingHero from '../Components/GreetingHero/GreetingHero'
 import NavBar from '../Components/Navbar/NavBar'
+import ProfilePicture from '../Components/ProfilePicture/ProfilePicture'
 
 const ProfilePage = () => {
-    return (
-        <>
-            <NavBar currentPage='Profile'/>
-            <GreetingHero/>
-        </>
-    )
-}   
+  return (
+    <>
+      <NavBar currentPage="Profile" />
+      <GreetingHero />
+      <ProfilePicture />
+    </>
+  )
+}
 
 export default ProfilePage
